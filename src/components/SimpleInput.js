4c280edd352efd9f159f45157990562ae62f10b6
MichalStabrawa <mistaaqua@gmail.com>
2023-03-05 23:24:15 +0100

    invalid class div

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react'
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('')
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
   }
@@ -19,9 +19,11 @@ const SimpleInput = (props) => {
     console.log(enteredName)
     setEnteredName('')
   }
+
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
         {!enteredNameIsValid && <p className='error-text'>Name must not be empty</p>}

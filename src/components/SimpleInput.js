6041efeed8ae609a9d14ed20db9b29e3d3a94a88
MichalStabrawa <mistaaqua@gmail.com>
2023-03-08 23:34:26 +0100

    nameInputHandler changes

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -8,13 +8,17 @@ const SimpleInput = (props) => {
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
+    if (event.target.value.trim() !== '') {
+      setEnteredNameIsValid(true);
+
+    }
   }
 
   const nameInputBlurHandler = event => {
     setEnteredNameTouched(true)
     if (enteredName.trim() === '') {
       setEnteredNameIsValid(false);
-      return;
+
     }
   }
 
